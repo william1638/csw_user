7995ad0990ef7da9b558246508a9d2ebf084e2a7
myb858 <myb858@hotmail.com>
2016-01-25 14:39:25 +0800

    update

--- docs/schema/stdsms.sql
+++ docs/schema/stdsms.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
 --
--- Host: 115.29.140.31    Database: xnsms
+-- Host: 120.55.113.192    Database: xnsms
 -- ------------------------------------------------------
--- Server version	5.5.42-log
+-- Server version	5.5.45
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `tjc_sms_captcha`
+--
+
+DROP TABLE IF EXISTS `tjc_sms_captcha`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tjc_sms_captcha` (
+  `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `mobile` varchar(16) NOT NULL COMMENT '手机号',
+  `biz_type` varchar(4) NOT NULL COMMENT '业务类型',
+  `sms_captcha` varchar(8) NOT NULL COMMENT '验证码',
+  `status` varchar(2) NOT NULL COMMENT '状态',
+  `create_datetime` datetime NOT NULL COMMENT '创建时间',
+  `sent_datetime` datetime DEFAULT NULL COMMENT '发送时间',
+  `invalid_datetime` datetime DEFAULT NULL COMMENT '失效时间',
+  `check_datetime` datetime DEFAULT NULL COMMENT '验证时间',
+  `check_times` int(11) DEFAULT NULL COMMENT '验证次数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `tjc_sms_out`
 --
 
@@ -23,14 +45,14 @@ DROP TABLE IF EXISTS `tjc_sms_out`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tjc_sms_out` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `mobile` varchar(16) NOT NULL COMMENT '手机号',
   `content` varchar(255) NOT NULL COMMENT '短信内容',
   `biz_type` varchar(2) DEFAULT NULL COMMENT '业务类型',
   `remark` varchar(255) DEFAULT NULL COMMENT '备注',
-  `status` varchar(1) NOT NULL COMMENT '状态',
-  `create_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-  `sent_datetime` timestamp NULL DEFAULT NULL COMMENT '发送时间',
+  `status` varchar(2) NOT NULL COMMENT '状态',
+  `create_datetime` datetime NOT NULL COMMENT '创建时间',
+  `sent_datetime` datetime DEFAULT NULL COMMENT '发送时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -44,4 +66,4 @@ CREATE TABLE `tjc_sms_out` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-09-23 10:30:02
+-- Dump completed on 2016-01-25 14:32:14
