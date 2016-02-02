34e8f60fc37a748f7ab86f282cf53e5c7da17074
myb858 <myb858@hotmail.com>
2016-02-02 15:30:38 +0800

    UPDATE

--- docs/schema/xnsms.sql
+++ docs/schema/xnsms.sql
@@ -1,27 +1,6 @@
--- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
---
--- Host: 120.55.113.192    Database: xnsms
--- ------------------------------------------------------
--- Server version	5.5.45
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `tjc_sms_captcha`
---
 
 DROP TABLE IF EXISTS `tjc_sms_captcha`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tjc_sms_captcha` (
   `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `mobile` varchar(16) NOT NULL COMMENT '手机号',
@@ -34,16 +13,10 @@ CREATE TABLE `tjc_sms_captcha` (
   `check_datetime` datetime DEFAULT NULL COMMENT '验证时间',
   `check_times` int(11) DEFAULT NULL COMMENT '验证次数',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `tjc_sms_out`
---
 
 DROP TABLE IF EXISTS `tjc_sms_out`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tjc_sms_out` (
   `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `mobile` varchar(16) NOT NULL COMMENT '手机号',
@@ -54,16 +27,4 @@ CREATE TABLE `tjc_sms_out` (
   `create_datetime` datetime NOT NULL COMMENT '创建时间',
   `sent_datetime` datetime DEFAULT NULL COMMENT '发送时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-02-02 15:27:41
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
