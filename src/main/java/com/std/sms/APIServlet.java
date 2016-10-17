8bd631e483c73d59031f0f5ba0755011b1e2c10e
pyez1158 <pyez1158@163.com>
2016-10-17 20:04:38 +0800

    脚本更新

--- src/main/java/com/std/sms/APIServlet.java
+++ src/main/java/com/std/sms/APIServlet.java
@@ -41,7 +41,7 @@ public class APIServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:1.0.0 fifth\n");
+        writer.append("Version:1.0.0 first\n");
         writer.flush();
     }
 
