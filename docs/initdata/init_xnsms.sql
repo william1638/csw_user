b02c648248280c06420f0169abdce67a16a26a9d
pyez1158 <pyez1158@163.com>
2016-10-17 20:10:55 +0800

    脚本更新

---
+++ docs/initdata/init_xnsms.sql
@@ -0,0 +1,18 @@
+-- ----------------------------
+-- Records of tjc_company
+-- ----------------------------
+INSERT INTO `tjc_company` VALUES ('GJ1002', '个金投资有限公司', '个金所');
+INSERT INTO `tjc_company` VALUES ('SYJ1003', '生意家', '生意家');
+
+
+-- ----------------------------
+-- Records of tjc_configure
+-- ----------------------------
+INSERT INTO `tjc_configure` VALUES ('1', 'GJ1002', 'CSMD', 'csmd_sn', 'SDK-JDH-010-00046', 'account');
+INSERT INTO `tjc_configure` VALUES ('2', 'GJ1002', 'CSMD', 'csmd_password', 'df9f0e__', 'password');
+INSERT INTO `tjc_configure` VALUES ('3', 'SYJ1003', 'HHXX', 'hhxx_userid_1', '48', 'userid');
+INSERT INTO `tjc_configure` VALUES ('4', 'SYJ1003', 'HHXX', 'hhxx_account_1', 'hh8004', 'account');
+INSERT INTO `tjc_configure` VALUES ('5', 'SYJ1003', 'HHXX', 'hhxx_password_1', 'hh8004', 'password');
+INSERT INTO `tjc_configure` VALUES ('6', 'SYJ1003', 'HHXX', 'hhxx_userid_2', '131', 'userid');
+INSERT INTO `tjc_configure` VALUES ('7', 'SYJ1003', 'HHXX', 'hhxx_account_2', 'hh8044', 'account');
+INSERT INTO `tjc_configure` VALUES ('8', 'SYJ1003', 'HHXX', 'hhxx_password_2', 'a123456', 'password');
