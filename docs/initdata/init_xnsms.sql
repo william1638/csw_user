6a028915363e7c97e75821f7ca92ceacb932f039
pyez1158 <pyez1158@163.com>
2016-10-18 21:27:39 +0800

    脚本更新

--- docs/initdata/init_xnsms.sql
+++ docs/initdata/init_xnsms.sql
@@ -1,19 +1,19 @@
 INSERT INTO `tjc_company` VALUES ('GJ1002', '个金投资有限公司', '个金所');
-INSERT INTO `tjc_configure` VALUES ('1', 'GJ1002', 'CSMD', 'csmd_sn', 'SDK-JDH-010-00046', 'account');
-INSERT INTO `tjc_configure` VALUES ('2', 'GJ1002', 'CSMD', 'csmd_password', 'df9f0e__', 'password');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`)  VALUES ('GJ1002', 'CSMD', 'csmd_sn', 'SDK-JDH-010-00046', 'account');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`)  VALUES ('GJ1002', 'CSMD', 'csmd_password', 'df9f0e__', 'password');
 
 INSERT INTO `tjc_company` VALUES ('SYJ1003', '生意家', '生意家');
-INSERT INTO `tjc_configure` VALUES ('3', 'SYJ1003', 'HHXX', 'hhxx_userid_1', '48', 'userid');
-INSERT INTO `tjc_configure` VALUES ('4', 'SYJ1003', 'HHXX', 'hhxx_account_1', 'hh8004', 'account');
-INSERT INTO `tjc_configure` VALUES ('5', 'SYJ1003', 'HHXX', 'hhxx_password_1', 'hh8004', 'password');
-INSERT INTO `tjc_configure` VALUES ('6', 'SYJ1003', 'HHXX', 'hhxx_userid_2', '131', 'userid');
-INSERT INTO `tjc_configure` VALUES ('7', 'SYJ1003', 'HHXX', 'hhxx_account_2', 'hh8044', 'account');
-INSERT INTO `tjc_configure` VALUES ('8', 'SYJ1003', 'HHXX', 'hhxx_password_2', 'a123456', 'password');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`)  VALUES ('SYJ1003', 'HHXX', 'hhxx_userid_1', '48', 'userid');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`)  VALUES ('SYJ1003', 'HHXX', 'hhxx_account_1', 'hh8004', 'account');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`)  VALUES ('SYJ1003', 'HHXX', 'hhxx_password_1', 'hh8004', 'password');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`)  VALUES ('SYJ1003', 'HHXX', 'hhxx_userid_2', '131', 'userid');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`)  VALUES ('SYJ1003', 'HHXX', 'hhxx_account_2', 'hh8044', 'account');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`)  VALUES ('SYJ1003', 'HHXX', 'hhxx_password_2', 'a123456', 'password');
 
 INSERT INTO `tjc_company` VALUES ('CSW1004', '温州城市网', '城市网');
-INSERT INTO `tjc_configure` VALUES ('9', 'CSW1004', 'HHXX', 'hhxx_userid_1', '162', 'userid');
-INSERT INTO `tjc_configure` VALUES ('10', 'CSW1004', 'HHXX', 'hhxx_account_1', 'hh8070', 'account');
-INSERT INTO `tjc_configure` VALUES ('11', 'CSW1004', 'HHXX', 'hhxx_password_1', 'ahh8070', 'password');
-INSERT INTO `tjc_configure` VALUES ('12', 'CSW1004', 'HHXX', 'hhxx_userid_2', '162', 'userid');
-INSERT INTO `tjc_configure` VALUES ('13', 'CSW1004', 'HHXX', 'hhxx_account_2', 'hh8070', 'account');
-INSERT INTO `tjc_configure` VALUES ('14', 'CSW1004', 'HHXX', 'hhxx_password_2', 'ahh8070', 'password');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CSW1004', 'HHXX', 'hhxx_userid_1', '162', 'userid');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CSW1004', 'HHXX', 'hhxx_account_1', 'hh8071', 'account');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CSW1004', 'HHXX', 'hhxx_password_1', 'ahh8070', 'password');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CSW1004', 'HHXX', 'hhxx_userid_2', '162', 'userid');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CSW1004', 'HHXX', 'hhxx_account_2', 'hh8071', 'account');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CSW1004', 'HHXX', 'hhxx_password_2', 'ahh8070', 'password');
