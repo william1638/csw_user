ce104ae3004adca24deb730456c1b416d694a5ff
asus <asus@DESKTOP-N61H2QE>
2017-04-23 17:14:40 +0800

    城市网

--- docs/initdata/init_std_sms.sql
+++ docs/initdata/init_std_sms.sql
@@ -5,6 +5,9 @@ INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES (
 INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CD-CCSW000007', 'HHXX', 'hhxx_userid_2', '162', 'userid');
 INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CD-CCSW000007', 'HHXX', 'hhxx_account_2', 'hh8070', 'account');
 INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CD-CCSW000007', 'HHXX', 'hhxx_password_2', 'ahh8070', 'password');
+INSERT INTO `tstd_system_channel` (`system_code`,`channel_type`,`push_type`,`status`,`push_system`,`private_key1`,`private_key2`,`private_key3`,`remark`) VALUES ('CD-CCSW000008','1','12','1','','162','hh8070','ahh8070','城市网');
+
+
 
 INSERT INTO `tjc_company` VALUES ('CD-CGD000006', '管道', '个金所');
 INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`)  VALUES ('CD-CGD000006', 'CSMD', 'csmd_sn', 'SDK-JDH-010-00046', 'account');
